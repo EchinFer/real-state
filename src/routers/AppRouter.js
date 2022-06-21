4e5e956698dba32cb202326c0651fd05a7d2427e
Moochfer <echinfer@gmail.com>
2022-06-21 05:46:23 -0400

    publish 3

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import ContactPage from '../components/Contact'
 import Home from '../components/Home'
 import PropertyDetails from '../components/property-details'
@@ -9,13 +9,14 @@ import PropertyPage from '../components/section-components/PropertyPage'
 
 export const AppRouter = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
                 <Route path="/properties" element={<PropertyPage />} />
                 <Route path="/property-details" element={<PropertyDetails />} />
                 <Route path="/contact" element={<ContactPage />} />
+                
+                <Route path="/" element={<Home />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
